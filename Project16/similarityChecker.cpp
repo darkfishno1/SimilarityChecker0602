7d889cf507d491491f5e3810928dd4ab23f2a1f3
hyunsung.jin <darkfishno1@gmail.com>
2023-06-02 11:48:56 +0900

    Exception unit test code 추가

--- Project16/similarityChecker.cpp
+++ Project16/similarityChecker.cpp
@@ -1,5 +1,34 @@
+#include <string>
+#include <stdexcept>
+using namespace std;
+
 class SimilarityChecker
 {
+
 public:
-	
+	int checkStr(string str1, string str2)
+	{
+		assertIllegalArgument(str1);
+		assertIllegalArgument(str2);
+
+
+		return 0;
+	}
+private:
+	bool isUpperCaseAlphaBet(string inputStr) {
+		for (char ch : inputStr) {
+			if (ch >= 'A' && ch <= 'Z') continue;
+			return true;
+		}
+		return false;
+	}
+
+	void assertIllegalArgument(string inputStr) {
+		if (inputStr == "") {
+			throw std::invalid_argument("값을 넣어야함");
+		}
+		if (isUpperCaseAlphaBet(inputStr)) {
+			throw std::invalid_argument("대문자알파벳이 아님");
+		}
+	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,14 @@
 #include "pch.h"
+#include "../Project16/similarityChecker.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+
+TEST(SimilarityCheckTestCase, InvalidArgument) {
+	SimilarityChecker checker;
+	EXPECT_THROW(checker.checkStr("ABS","123"), std::invalid_argument);
+	EXPECT_THROW(checker.checkStr("ABS", "ABc"), std::invalid_argument);
+}
+
+TEST(SimilarityCheckTestCase, CheckLength) {
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
 }
